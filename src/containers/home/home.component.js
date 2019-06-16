3fdd56cf0f589e194b20e921e08169b9333c3fa1
Artur Yorsh <artyorsh.me@gmail.com>
2019-06-16 19:29:27 +0300

    make examples even easier

--- src/containers/home/home.component.js
+++ src/containers/home/home.component.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { Button, Text, Layout } from 'react-native-ui-kitten';
+import {
+  Button,
+  Layout,
+  Text,
+} from 'react-native-ui-kitten';
 
-export const Home = (props) => {
-
-  const onToggleTheme = () => {
-    props.toggleTheme();
-  };
-
-  return (
-    <Layout style={styles.container}>
-      <Text>Use Dark Mode?</Text>
-      <Button onPress={onToggleTheme}>Yeah 🌚</Button>
-      <Button onPress={onToggleTheme}>Nope 🌞</Button>
-    </Layout>
-  );
-};
+export const Home = (props) => (
+  <Layout style={styles.container}>
+    <Text category='h4'>Welcome to UI Kitten</Text>
+    <Button onPress={props.toggleTheme}>Toggle Dark Mode 🌚</Button>
+  </Layout>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'space-evenly',
   },
 });
